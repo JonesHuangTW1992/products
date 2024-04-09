4285a440a4dca677dff731d581848ede9993583f
JonesHuangTW1992 <160464004+JonesHuangTW1992@users.noreply.github.com>
2024-04-10 00:26:53 +0800

    add header

--- products.py
+++ products.py
@@ -7,6 +7,7 @@ while True:
 	if name == 'q': # quit
 		break
 	price = input('請輸入商品價格: ') # 寫在break下的原因，若名稱輸入q，則不會再跳出這句	
+	price = int(price) # 價格設為整數而非字串
 	products.append([name, price]) # 為下列的縮寫
 	# p = [] 
 	# p.append(name)
@@ -23,3 +24,19 @@ for p in products:
 	print(p)
 	print(p[0], '的價格是', p[1])
 
+# 字串可以合併
+# 'abc' + '123' = 'abc123'
+# 'abc' * '3' = 'abcabcabc'
+with open('products.csv', 'w', encoding= 'utf-8') as f:
+	
+	# encoding = 使用___編碼
+	# 打開並寫入 當作f 
+	# csv檔可以用excel打開
+	# w = write = 寫入, r = read = 讀取
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n')
+		# 用加法當作字串的合併，上面等於四個字串相加, 而後丟給f.write這個功能
+		# str = string 改為字串
+		# ',' 氏為了在csv檔內作分隔
+		# '\n' 為換行功能
